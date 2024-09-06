80cbf17d10eb5f2e0cef360ce2babf9fa6886aca
spooketti <jonliu168@gmail.com>
2024-09-06 08:59:20 -0700

    pushing for the beautiful sir tarun J

--- static/js/board.js
+++ static/js/board.js
@@ -7,6 +7,20 @@ const canvasWidth = boxDim * cols;
 const canvasHeight = boxDim * rows;
 let matrix = new Array(cols).fill(0).map(() => new Array(rows).fill(0));
 
+//start data
+//ypos, xpos, rot
+
+//rot 0 north, 1 east, 2 south, 3, west
+
+let P1StartData = [0,0,0]
+let P2StartData = [0,1,0]
+let EndPos = [19,19] //ypos xpos
+
+matrix[P1StartData[0]][P1StartData[1]] = 0.1
+matrix[P2StartData[0]][P2StartData[1]] = 0.2
+matrix[EndPos[0]][EndPos[1]] = 0.3
+let playerOne = new player()
+let playerTwo = new player()
 
 function initBoard() {
     ctx.strokeStyle = "rgb(0,0,0)"
@@ -21,6 +35,18 @@ function initBoard() {
         ctx.moveTo(boxDim * x, 0)
         ctx.lineTo(boxDim * x, canvasHeight)
         ctx.stroke()
+    }
+    cTX.fillRect((1 * 24) + 1, (1 * 24) + 1, 22, 22)
+}
+
+function drawBoard()
+{
+    for(let y=0;y<cols;y++)
+    {
+        for(let x=0;x<rows;x++)
+        {
+            ctx.clearRect((x*boxDim)+1,(y*boxDim)+1,boxDim-2,boxDim-2)
+        }
     }
 }
 
---
+++ static/js/player.js
@@ -0,0 +1,10 @@
+class player
+{
+    constructor(startX,startY,startRotation,color)
+    {
+        this.x = startX
+        this.y = startY
+        this.rotation = startRotation;
+        this.color = color
+    }
+}
